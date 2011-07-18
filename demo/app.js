3900aa29dc02c554225dc647376a8c241bc23c2a
tucaz <me@tucaz.net>
2011-07-18 14:12:14 -0300

    updated demo to reflect new API

--- demo/app.js
+++ demo/app.js
@@ -1,56 +1,32 @@
 $(document).ready( function () {
-    var $controlsContainer = $('#controls'),
-    $expressionContainer = $('#expression'),
-    data = [{
-        name: 'Roles',
-        prefix: '{r=',
-        sufix: '}',
-        values: ['RoleA', 'RoleB', 'RoleC']
-    },{
-        name: 'Groups',
-        prefix: '{g=',
-        sufix: '}',
-        values: ['Group1', 'Group2', 'Group3', 'Group4']
-    },{
-        name: 'Attributes',
-        prefix: '{a=',
-        sufix: '}',
-        values: ['Attr1', 'Attr2']
-    }],
-    editor = new expEd.Editor($expressionContainer, data);
+    var editor = new expEd.Editor({
+        expressionContainer:$('#expression'),
+        tokens: [{
+            name: 'Roles',
+            prefix: '{r=',
+            sufix: '}',
+            values: ['RoleA', 'RoleB', 'RoleC']
+        },{
+            name: 'Groups',
+            prefix: '{g=',
+            sufix: '}',
+            values: ['Group1', 'Group2', 'Group3', 'Group4']
+        },{
+            name: 'Attributes',
+            prefix: '{a=',
+            sufix: '}',
+            values: ['Attr1', 'Attr2']
+        }],
+        operators: ['AND','OR']
+    });
 
-    editor.createUI($controlsContainer);
+    editor.createUI($('#controls'));
 
     editor.onError( function(msg) {
         alert(msg)
     });
 
-    $('#add-or').click( function(e) {
-        editor.addToken('OR')
-    });
-
-    $('#add-and').click( function(e) {
-        editor.addToken('AND')
-    });
-
-    $('#group').click( function(e) {
-        editor.groupSelected()
-    });
-
     $('#show').click( function(e) {
         $('#text').text(editor.toString());
     });
-
-    $('#unselect').click( function(e) {
-        editor.unselectAll();
-    });
-
-    $('#remove').click( function(e) {
-        editor.removeSelected();
-    });
-
-    $('#clear-all').click( function(e) {
-        editor.clearAll();
-    });
-
 });
